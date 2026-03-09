5808996a5f152b7c41e05e3bc74a5d8f4924b88c
b.martinez <207059653+BernatMBorrell@users.noreply.github.com>
2026-03-09 11:31:28 +0000

    timing problems sorta fixed

--- ROS_ws/src/custom_slam/launch/simulation.launch.py
+++ ROS_ws/src/custom_slam/launch/simulation.launch.py
@@ -122,7 +122,7 @@ def generate_launch_description():
         actions=[spawn]
     )
     control_after_spawn = TimerAction(
-        period=5.0,  # seconds, adjust as needed
+        period=10.0,  # seconds, adjust as needed
         actions=[controller]
     )
 
